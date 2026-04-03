dc350f2d611bbabdcb3490d538472c288489ae3b
luis <gomesluis.dev@gmail.com>
2026-04-03 18:03:30 -0300

    update again schema config

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '../generated/client';
+import { PrismaClient } from '../generated/prisma/client';
 import { Pool } from 'pg';
 import { PrismaPg } from '@prisma/adapter-pg';
 
